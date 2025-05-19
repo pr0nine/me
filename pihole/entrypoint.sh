78b44ed7143e2efaede77d41e9196704aafb6a2d
pr0nine <100036997+pr0nine@users.noreply.github.com>
2025-05-19 11:06:44 +0530

    Update entrypoint.sh
    starting script for unbound

--- pihole/entrypoint.sh
+++ pihole/entrypoint.sh
@@ -3,3 +3,8 @@
 mkdir -p /var/log/unbound
 #give ownership to pihole
 chown pihole:pihole /var/log/unbound
+
+#start Unbound with error checking
+echo " [i] Starting Unbound"
+#with config mention in https://docs.pi-hole.net/guides/dns/unbound/ 
+/usr/sbin/unbound -d -c /etc/unbound/unbound.conf.d/pi-hole.conf & UNBOUND_PID=$!
